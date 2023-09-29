7d6fddd4a8fc9cb26bd3615d36e6b23940bdaa11
Mike Bailey <michael.l.bailey@hotmail.com>
2023-09-29 07:17:09 -0400

    Update Scriptsensor.ino

--- Scriptsensor.ino
+++ Scriptsensor.ino
@@ -1,80 +1,49 @@
 #include <Wire.h>
 
-const int I2C_ADDRESS = 0x12;
-const int pathos_lowerPin = A0;
-const int pathos_upperPin = A1;
-const int palmPin = A2;
-const long readingInterval = 5000; // Update every 5 seconds
+const int sensorCount = 3;  // Number of sensors
+int AirValue[sensorCount] = {501, 494, 500};  // Replace with appropriate values for each sensor
+int WaterValue[sensorCount] = {217, 216, 200}; // Replace with appropriate values for each sensor
 
-unsigned long previousMillisPathosLower = 0;
-unsigned long previousMillisPathosUpper = 1000;  // Delayed start
-unsigned long previousMillisPalm = 2000;         // Delayed start
-int pathos_lowerReading, pathos_upperReading, palmReading;
-
-const int pathos_lowerDry = 100;
-const int pathos_lowerWet = 0;
-const int pathos_upperDry = 100;
-const int pathos_upperWet = 0;
-const int palmDry = 100;
-const int palmWet = 0;
+int soilMoistureValue[sensorCount] = {0};
+int soilMoisturePercent[sensorCount] = {0};
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin(I2C_ADDRESS);
-  Wire.onRequest(sendReadings);
-  pinMode(pathos_lowerPin, INPUT);
-  pinMode(pathos_upperPin, INPUT);
-  pinMode(palmPin, INPUT);
+  Serial.begin(9600); // Open serial port, set the baud rate to 9600 bps
+  Wire.begin(8);      // Start I2C communication as a slave with address 8
+  Wire.onRequest(sendData); // Register callback for I2C request
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillisPathosLower >= readingInterval) {
-    previousMillisPathosLower = currentMillis;
-
-    pathos_lowerReading = analogRead(pathos_lowerPin);
-    pathos_lowerReading = map(constrain(pathos_lowerReading, pathos_lowerWet, pathos_lowerDry), pathos_lowerWet, pathos_lowerDry, 100, 0);
-
-    // Output the sensor reading to the Serial Monitor
-    Serial.print("Pothos Lower Sensor Reading: ");
-    Serial.println(pathos_lowerReading);
+  // Read sensor values and calculate percentages for each sensor
+  for (int i = 0; i < sensorCount; i++) {
+    soilMoistureValue[i] = analogRead(A0 + i);
+    soilMoisturePercent[i] = map(soilMoistureValue[i], AirValue[i], WaterValue[i], 0, 100);
   }
 
-  if (currentMillis - previousMillisPathosUpper >= readingInterval) {
-    previousMillisPathosUpper = currentMillis;
-
-    pathos_upperReading = analogRead(pathos_upperPin);
-    pathos_upperReading = map(constrain(pathos_upperReading, pathos_upperWet, pathos_upperDry), pathos_upperWet, pathos_upperDry, 100, 0);
-
-    // Output the sensor reading to the Serial Monitor
-    Serial.print("Pothos Upper Sensor Reading: ");
-    Serial.println(pathos_upperReading);
+  // Output data to serial monitor for debugging
+  for (int i = 0; i < sensorCount; i++) {
+    Serial.print("Sensor ");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    if (soilMoisturePercent[i] >= 100) {
+      Serial.println("100 %");
+    } else if (soilMoisturePercent[i] <= 0) {
+      Serial.println("0 %");
+    } else {
+      Serial.print(soilMoisturePercent[i]);
+      Serial.println("%");
+    }
   }
-
-  if (currentMillis - previousMillisPalm >= readingInterval) {
-    previousMillisPalm = currentMillis;
-
-    palmReading = analogRead(palmPin);
-    palmReading = map(constrain(palmReading, palmWet, palmDry), palmWet, palmDry, 100, 0);
-
-    // Output the sensor reading to the Serial Monitor
-    Serial.print("Palm Sensor Reading: ");
-    Serial.println(palmReading);
-  }
+  
+  delay(500); // Update sensor data every 500ms
 }
 
-void sendReadings() {
-  byte data[12]; // 3 sensors * 4 bytes each
-
-  // Store sensor readings in bytes
-  data[0] = highByte(pathos_lowerReading);
-  data[1] = lowByte(pathos_lowerReading);
-  data[2] = highByte(pathos_upperReading);
-  data[3] = lowByte(pathos_upperReading);
-  data[4] = highByte(palmReading);
-  data[5] = lowByte(palmReading);
-
-  // Send the structured data over I2C
-  Wire.write(data, 12);
+// Callback function to send data over I2C when requested
+void sendData() {
+  for (int i = 0; i < sensorCount; i++) {
+    byte data[2];
+    data[0] = soilMoisturePercent[i] >> 8; // MSB
+    data[1] = soilMoisturePercent[i] & 0xFF; // LSB
+    Wire.write(data, 2);
+  }
 }
